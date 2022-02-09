f8bc5effe29515fcbf814863ded885bca2618aba
VasiliyBologov <vasiliybologov@gmail.com>
2022-02-09 18:41:10 +0200

    refactoring

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
-import flask as flask
-from flask import Flask
+import flask
+from flask import json
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
 
 @app.route("/")
 def library_home():
     return flask.render_template('library_home.html')
 
-@app.route('/add_position')
-def add_position():
-    return flask.render_template('add_position.html')
+
+@app.route('/add-book', methods=['GET','POST'])
+def add_book():
+    if flask.request.method == "GET":
+        return flask.render_template('add_book.html')
+    if flask.request.method == "POST":
+        input_request = flask.request.form
+        print(input_request)
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
 
 @app.route('/library')
 def library():
@@ -17,4 +25,4 @@ def library():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ services.py
@@ -0,0 +1,7 @@
+def get_all_books():
+    pass
+
+
+def add_new_book():
+    pass
+
